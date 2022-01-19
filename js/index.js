78ef849be374996773a799a74f94ba8eaf71f1eb
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-19 11:55:43 -0400

    Uncomment code

--- js/index.js
+++ js/index.js
@@ -38,20 +38,19 @@ const months = [
   "Dec",
 ];
 
-// // Set initial color theme
-
-// if (
-//   window.matchMedia &&
-//   window.matchMedia("(prefers-color-scheme: light)").matches
-// ) {
-//   body.classList.add("light-mode");
-//   light.style.display = "none";
-//   dark.style.display = "flex";
-// } else {
-//   body.classList.add("dark-mode");
-//   dark.style.display = "none";
-//   light.style.display = "flex";
-// }
+// Set initial color theme
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches
+) {
+  body.classList.add("light-mode");
+  light.style.display = "none";
+  dark.style.display = "flex";
+} else {
+  body.classList.add("dark-mode");
+  dark.style.display = "none";
+  light.style.display = "flex";
+}
 
 const searchForUser = async (usernameInput) => {
   try {
